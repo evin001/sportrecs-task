c43c46707c7ab80b609633c632356fa44e2bd085
Evgeniy <blednovea@smartguide.agency>
2020-06-09 14:19:03 +0300

    root container

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { ApolloProvider } from '@apollo/react-hooks'
+import Container from '@material-ui/core/Container'
 import UserList from './components/UserList'
 
 const App = ({ client }) => (
   <ApolloProvider client={client}>
-    <UserList />
+    <Container maxWidth="md">
+      <UserList />
+    </Container>
   </ApolloProvider>
 )
 
